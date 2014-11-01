e1ce7aee5abe30cca90b9d6f90be4c74ee1c6751
ppaquay <p.paquay@linuxaddict.be>
2014-11-01 19:30:41 +0100

    Modif in data cleaning

--- Analysis.R
+++ Analysis.R
@@ -7,7 +7,7 @@ library(wordcloud)
 
 preproc.pruned <- tm_map(preproc, removeWords, stopwords("english"))
 #sampleTDM.unigrams <- TermDocumentMatrix(preproc.pruned)
-sampleTDM.unigrams <- TermDocumentMatrix(preproc.pruned, control = list(tokenize = UnigramTokenizer))
+sampleTDM.onegrams <- TermDocumentMatrix(preproc.pruned, control = list(tokenize = OnegramTokenizer))
 sampleTDM.common <- removeSparseTerms(sampleTDM, 0.6)
 
 # 1.1. Frequent unigrams
@@ -16,10 +16,10 @@ sampleTDM.common <- removeSparseTerms(sampleTDM, 0.6)
 #ord <- order(freq.unigrams)
 #freq.unigrams[tail(ord)]
 #head(table(freq.unigrams), 20)
-freq.unigrams <- sort(rowSums(as.matrix(sampleTDM.unigrams)), decreasing = TRUE)
-head(freq.unigrams, 20)
-head(table(freq.unigrams), 20)
-findFreqTerms(sampleTDM.unigrams, lowfreq = 50)
+freq.onegrams <- sort(rowSums(as.matrix(sampleTDM.onegrams)), decreasing = TRUE)
+head(freq.onegrams, 20)
+head(table(freq.onegrams), 20)
+findFreqTerms(sampleTDM.onegrams, lowfreq = 50)
 
 # 1.1.1. How many unique words do you need in a frequency sorted dictionary to cover 50% of all word instances in the language? 90%?
 
@@ -32,65 +32,65 @@ coverInstance <- function(freq, threshold) {
     }
     return(i)
 }
-coverInstance(freq.unigrams, 50)
-coverInstance(freq.unigrams, 90)
+coverInstance(freq.onegrams, 50)
+coverInstance(freq.onegrams, 90)
 
 # 1.2. Graph of most frequent unigrams
 
-freq.term.unigrams <- subset(freq.unigrams, freq.unigrams >= 600)
-df <- data.frame(term = names(freq.term.unigrams), freq = freq.term.unigrams)
+freq.term.onegrams <- subset(freq.onegrams, freq.onegrams >= 600)
+df <- data.frame(term = names(freq.term.onegrams), freq = freq.term.onegrams)
 ggplot(df, aes(x = term, y = freq)) + geom_bar(stat = "identity") + xlab("Terms") + ylab("Count") + theme(axis.text.x=element_text(angle=45, hjust=1))
-df <- data.frame(term = as.numeric(names(table(freq.unigrams))), freq = as.vector(table(freq.unigrams)))
+df <- data.frame(term = as.numeric(names(table(freq.onegrams))), freq = as.vector(table(freq.onegrams)))
 ggplot(df, aes(x = log(term), y = log(freq))) + layer(geom = "point")
 plot(log(df$term), log(df$freq))
 
 # 1.3. Associations (not useful ?!)
 
-findAssocs(sampleTDM.unigrams, "into", corlimit = 0.98)
+findAssocs(sampleTDM.onegrams, "into", corlimit = 0.98)
 
 # 1.4. Word cloud of unigrams
 
 set.seed(1)
-wordcloud(names(freq.unigrams), freq.unigrams, max.words = 100, colors = brewer.pal(6, "Dark2"))
+wordcloud(names(freq.onegrams), freq.onegrams, max.words = 100, colors = brewer.pal(6, "Dark2"))
 
 # 2. Document Term Matrix for bigrams
 
-sampleTDM.bigrams <- TermDocumentMatrix(preproc, control = list(tokenize = BigramTokenizer))
+sampleTDM.twograms <- TermDocumentMatrix(preproc, control = list(tokenize = TwogramTokenizer))
 
 # 2.1. Frequent bigrams
 
-freq.bigrams <- sort(rowSums(as.matrix(sampleTDM.bigrams)), decreasing = TRUE)
-head(table(freq.bigrams), 20)
-head(freq.bigrams, 20)
-findFreqTerms(sampleTDM.bigrams, lowfreq = 50)
+freq.twograms <- sort(rowSums(as.matrix(sampleTDM.twograms)), decreasing = TRUE)
+head(table(freq.twograms), 20)
+head(freq.twograms, 20)
+findFreqTerms(sampleTDM.twograms, lowfreq = 50)
 
 # 2.2. Graph of most frequent bigrams
 
-freq.term.bigrams <- subset(freq.bigrams, freq.bigrams >= 600)
-df <- data.frame(term = names(freq.term.bigrams), freq = freq.term.bigrams)
+freq.term.twograms <- subset(freq.twograms, freq.twograms >= 600)
+df <- data.frame(term = names(freq.term.twograms), freq = freq.term.twograms)
 ggplot(df, aes(x = term, y = freq)) + geom_bar(stat = "identity") + xlab("Terms") + ylab("Count") + theme(axis.text.x=element_text(angle=45, hjust=1))
 
 # 2.3. Word cloud of bigrams
 
-wordcloud(names(freq.bigrams), freq.bigrams, max.words = 100, colors = brewer.pal(6, "Dark2"))
+wordcloud(names(freq.twograms), freq.twograms, max.words = 100, colors = brewer.pal(6, "Dark2"))
 
 # 3. Document Term Matrix for trigrams
 
-sampleTDM.trigrams <- TermDocumentMatrix(preproc, control = list(tokenize = TrigramTokenizer))
+sampleTDM.threegrams <- TermDocumentMatrix(preproc, control = list(tokenize = ThreegramTokenizer))
 
 # 3.1. Frequent trigrams
 
-freq.trigrams <- sort(rowSums(as.matrix(sampleTDM.trigrams)), decreasing = TRUE)
-head(table(freq.trigrams), 20)
-head(freq.trigrams, 20)
-findFreqTerms(sampleTDM.trigrams, lowfreq = 50)
+freq.threegrams <- sort(rowSums(as.matrix(sampleTDM.threegrams)), decreasing = TRUE)
+head(table(freq.threegrams), 20)
+head(freq.threegrams, 20)
+findFreqTerms(sampleTDM.threegrams, lowfreq = 50)
 
 # 3.2. Graph of most frequent trigrams
 
-freq.term.trigrams <- subset(freq.trigrams, freq.trigrams >= 50)
-df <- data.frame(term = names(freq.term.trigrams), freq = freq.term.trigrams)
+freq.term.threegrams <- subset(freq.threegrams, freq.threegrams >= 50)
+df <- data.frame(term = names(freq.term.threegrams), freq = freq.term.threegrams)
 ggplot(df, aes(x = term, y = freq)) + geom_bar(stat = "identity") + xlab("Terms") + ylab("Count") + theme(axis.text.x=element_text(angle=45, hjust=1))
 
 # 3.3. Word cloud of trigrams
 
-wordcloud(names(freq.trigrams), freq.trigrams, max.words = 40, colors = brewer.pal(6, "Dark2"))
+wordcloud(names(freq.threegrams), freq.threegrams, max.words = 39, colors = brewer.pal(6, "Dark2"))
--- DataCleaning2.R
+++ DataCleaning2.R
@@ -32,18 +32,20 @@ preproc <- corpus.sample
 
 # 4.1. Abbreviations removing
 
-new.abbr <- data.frame(abv = c("Mr.", "Mrs.", "Ms.", "i.e.", "A.D.", "B.C.", "A.M.", "P.M.", "et al.", "Jr.", "Dr.", "Sr.", "Sen.", "U.S.", "U.S.A.", "p.s.", "w/", "&"), rep = c("Mr", "Mrs", "Ms", "ie", "AD", "BC", "AM", "PM", "et al", "Jr", "Dr", "Sr", "Sen", "USA", "USA", "ps", "with", "and"))
+new.abbr <- data.frame(abv = c("Mr.", "Mrs.", "Ms.", "i.e.", "A.D.", "B.C.", "A.M.", "P.M.", "et al.", "Jr.", "Dr.", "Sr.", "Sen.", "U.S.", "U.S.A.", "p.s.", "w/", "&", "$"), rep = c("Mr", "Mrs", "Ms", "ie", "AD", "BC", "AM", "PM", "et al", "Jr", "Dr", "Sr", "Sen", "USA", "USA", "ps", "with", "and", "dollar "))
 preproc <- tm_map(preproc, content_transformer(replace_abbreviation), abbreviation = new.abbr)
 
 # 4.2. Ordinals and numbers removing
 
 preproc <- tm_map(preproc, content_transformer(replace_ordinal), num.paste = FALSE)
-preproc <- tm_map(preproc, content_transformer(replace_number))
+preproc <- tm_map(preproc, content_transformer(removeNumbers))
 
 # 4.3. Remove URLs
 
-removeURL <- function(x) gsub("www.[A-z0-9]+.[a-z]{2,}", "", x)
-preproc <- tm_map(preproc, content_transformer(removeURL))
+removeURL1 <- function(x) gsub("http://", "", x)
+preproc <- tm_map(preproc, content_transformer(removeURL1))
+removeURL2 <- function(x) gsub("www.[A-z0-9]+.[a-z]{2,}", "", x)
+preproc <- tm_map(preproc, content_transformer(removeURL2))
 
 # 4.4. Remove brackets
 
@@ -61,16 +63,16 @@ preproc <- tm_map(preproc, content_transformer(removeBrackets))
 removeHashtags <- function(x) gsub("#\\w+", "", x)
 preproc <- tm_map(preproc, content_transformer(removeHashtags))
 
-# 4.6. Remove intra word dashes and dollar signs
+# 4.6. Remove intra word dashes
 
-removeDashes <- function(x) gsub("-+", " ", x)
+removeDashes <- function(x) gsub("-+|/", " ", x)
 preproc <- tm_map(preproc, content_transformer(removeDashes))
-removeDollar <- function(x) gsub("\\$", "", x)
-preproc <- tm_map(preproc, content_transformer(removeDollar))
+#removeDollar <- function(x) gsub("\\$", "", x)
+#preproc <- tm_map(preproc, content_transformer(removeDollar))
 
 # 4.7. Sentence splitting
 
-preproc <- tm_map(preproc, content_transformer(sent_detect), endmarks = c("?", ".", "!", "|", "…", ":"), language = "en")
+preproc <- tm_map(preproc, content_transformer(sent_detect), endmarks = c("?", ".", "!", "|", "…", ":", "\n","\r\n"), language = "en")
 
 # 4.8. Lower casing
 
@@ -82,18 +84,19 @@ preproc <- tm_map(preproc, content_transformer(iconv), from = "latin1", to = "AS
 
 # 4.10. Punctuation removing
 
-preproc <- tm_map(preproc, content_transformer(removePunctuation))
+removePunc <- function(x) gsub("\\.|:|!|\\?|\\||_|(|)|,|;|=|\\*", "", x)
+preproc <- tm_map(preproc, content_transformer(removePunc))
 
-# 4.10. Trailing spaces removing
+# 4.11. Trailing spaces removing
 
 removeTrailingSpaces <- function(x) gsub("^\\s|\\s$", "", x)
 preproc <- tm_map(preproc, content_transformer(removeTrailingSpaces))
 
-# 4.11. White space removing
+# 4.12. White space removing
 
 preproc <- tm_map(preproc, stripWhitespace)
 
-# 4.12 Empty strings removing
+# 4.13 Empty strings removing
 
 removeEmptyStrings <- function(x) {
     return(x[x != ""])
@@ -102,31 +105,31 @@ preproc <- tm_map(preproc, content_transformer(removeEmptyStrings))
 
 # 5. Profanity filtering
 
-profanities <- c("acrotomophilia", "anal", "anilingus", "anus", "arsehole", "ass", "asses", "asshole", "assholes", "assmunch", "auto erotic", "autoerotic", "babeland", "baby batter", "ball gag", "ball gravy", "ball kicking", "ball licking", "ball sack", "ball sucking", "bangbros", "bareback", "barely legal", "barenaked", "bastardo", "bastinado", "bbw", "bdsm", "beaver cleaver", "beaver lips", "bestiality", "bi curious", "big black", "big breasts", "big knockers", "big tits", "bimbos", "birdlock", "bitch", "bitches", "black cock", "blonde action", "blonde on blonde action", "blow j", "blow your l", "blue waffle", "blumpkin", "bollocks", "bondage", "boner", "boob", "boobs", "booty call", "brown showers", "brunette action", "bukkake", "bulldyke", "bullet vibe", "bung hole", "bunghole", "busty", "butt", "butts", "buttcheeks", "butthole", "camel toe", "cameltoe", "cameltoes", "camgirl", "camslut", "camwhore", "carpet muncher", "carpetmuncher", "chocolate rosebuds", "circlejerk", "cleveland steamer", "clit", "clitoris", "clover clamps", "clusterfuck", "cock", "cocks", "coprolagnia", "coprophilia", "cornhole", "cum", "cumming", "cunnilingus", "cunt", "cunts", "darkie", "date rape", "daterape", "deep throat", "deepthroat", "dick", "dicks", "dildo", "dirty pillows", "dirty sanchez", "doggie style", "doggiestyle", "doggy style", "doggystyle", "dog style", "dolcett", "dominatrix", "dommes", "donkey punch", "double dong", "double penetration", "dp action", "eat my ass", "ecchi", "ejaculation", "erotic", "erotism", "escort", "ethical slut", "eunuch", "faggot", "fecal", "felch", "fellatio", "feltch", "female squirting", "femdom", "figging", "fingering", "fisting", "foot fetish", "footjob", "frotting", "fuck", "fuckit", "fucked", "fucker", "fuckers", "fuckin", "fucking", "fuckyou", "futanari", "gang bang", "gay sex", "genitals", "giant cock", "girl on", "girls gone wild", "gokkun", "goodpoop", "goo girl", "grope", "group sex", "g-spot", "guro", "hand job", "handjob", "hard core", "hardcore", "hentai", "hooker", "hot chick", "huge fat", "humping", "incest", "intercourse", "jack off", "jail bait", "jailbait", "jerk off", "jigaboo", "jiggaboo", "jiggerboo", "jizz", "juggs", "kike", "kinbaku", "kinkster", "kinky", "knobbing", "leather restraint", "leather straight jacket", "lemon party", "lolita", "lovemaking", "make me come", "male squirting", "masturbate", "menage a trois", "milf", "missionary position", "motherfucker", "motherfuckers", "mound of venus", "mr hands", "muff diver", "muffdiving", "nambla", "nawashi", "negro", "neonazi", "nigga", "niggas", "nigger", "niggers", "nig nog", "nimphomania", "nipple", "nipples", "nsfw images", "nude", "nudity", "nympho", "nymphomania", "octopussy", "omorashi", "orgasm", "orgy", "orgies", "paedophile", "panties", "panty", "pedobear", "pedophile", "pegging", "penis", "phone sex", "piece of shit", "pissing", "piss pig", "pisspig", "playboy", "pleasure chest", "pole smoker", "ponyplay", "poof", "poopchute", "porn", "porno", "pornography", "pthc", "pubes", "pussy", "queaf", "raghead", "rape", "raping", "rapist", "rectum", "rimjob", "rimming", "rosy palm", "rusty trombone", "sadism", "scat", "schlong", "scissoring", "semen", "sex", "sexo", "sexy", "shaved beaver", "shaved pussy", "shemale", "shibari", "shit", "shota", "shrimping", "slanteye", "slut", "sluts", "s&m", "smut", "snatch", "sodomize", "sodomy", "spic", "spooge", "spread legs", "strap on", "strapon", "strip club", "style doggy", "suck", "sucks", "suicide girls", "sultry women", "swastika", "swinger", "tea bagging", "threesome", "throating", "tied up", "tight white", "tit", "tits", "titties", "titty", "topless", "tosser", "tranny", "tribadism", "tub girl", "tubgirl", "tushy", "twat", "twink", "twinkie", "undressing", "upskirt", "urethra play", "urophilia", "vagina", "venus mound", "vibrator", "violet wand", "vorarephilia", "voyeur", "vulva", "wank", "wetback", "wet dream", "white power", "women rapping", "wrapping men", "xx", "xxx", "yaoi", "yiffy", "zoophilia")
+profanities <- c("acrotomophilia", "anal", "anilingus", "anus", "arsehole", "ass", "asses", "asshole", "assholes", "assmunch", "auto erotic", "autoerotic", "babeland", "baby batter", "ball gag", "ball gravy", "ball kicking", "ball licking", "ball sack", "ball sucking", "bangbros", "bareback", "barely legal", "barenaked", "bastardo", "bastinado", "bbw", "bdsm", "beaver cleaver", "beaver lips", "bestiality", "bi curious", "big black", "big breasts", "big knockers", "big tits", "bimbos", "birdlock", "bitch", "bitches", "black cock", "blonde action", "blonde on blonde action", "blow j", "blow your l", "blue waffle", "blumpkin", "bollocks", "bondage", "boner", "boob", "boobs", "booty call", "brown showers", "brunette action", "bukkake", "bulldyke", "bullet vibe", "bung hole", "bunghole", "busty", "butt", "butts", "buttcheeks", "butthole", "camel toe", "cameltoe", "cameltoes", "camgirl", "camslut", "camwhore", "carpet muncher", "carpetmuncher", "chocolate rosebuds", "circlejerk", "cleveland steamer", "clit", "clitoris", "clover clamps", "clusterfuck", "cock", "cocks", "coprolagnia", "coprophilia", "cornhole", "cum", "cumming", "cunnilingus", "cunt", "cunts", "darkie", "date rape", "daterape", "deep throat", "deepthroat", "dick", "dicks", "dildo", "dirty pillows", "dirty sanchez", "doggie style", "doggiestyle", "doggy style", "doggystyle", "dog style", "dolcett", "dominatrix", "dommes", "donkey punch", "double dong", "double penetration", "dp action", "eat my ass", "ecchi", "ejaculation", "erotic", "erotism", "escort", "ethical slut", "eunuch", "faggot", "fecal", "felch", "fellatio", "feltch", "female squirting", "femdom", "figging", "fingering", "fisting", "foot fetish", "footjob", "frotting", "fuck", "fuck off", "fuckit", "fucked", "fucker", "fuckers", "fuckin", "fucking", "fuckyou", "futanari", "gang bang", "gay sex", "genitals", "giant cock", "girl on", "girls gone wild", "gokkun", "goodpoop", "goo girl", "grope", "group sex", "g-spot", "guro", "hand job", "handjob", "hard core", "hardcore", "hentai", "hooker", "hot chick", "huge fat", "humping", "incest", "intercourse", "jack off", "jail bait", "jailbait", "jerk off", "jigaboo", "jiggaboo", "jiggerboo", "jizz", "juggs", "kike", "kinbaku", "kinkster", "kinky", "knobbing", "leather restraint", "leather straight jacket", "lemon party", "lolita", "lovemaking", "make me come", "male squirting", "masturbate", "menage a trois", "milf", "missionary position", "motherfucker", "motherfuckers", "mound of venus", "mr hands", "muff diver", "muffdiving", "nambla", "nawashi", "negro", "neonazi", "nigga", "niggas", "nigger", "niggers", "nig nog", "nimphomania", "nipple", "nipples", "nsfw images", "nude", "nudity", "nympho", "nymphomania", "octopussy", "omorashi", "orgasm", "orgy", "orgies", "paedophile", "panties", "panty", "pedobear", "pedophile", "pegging", "penis", "phone sex", "piece of shit", "pissing", "piss pig", "pisspig", "playboy", "pleasure chest", "pole smoker", "ponyplay", "poof", "poopchute", "porn", "porno", "pornography", "pthc", "pubes", "pussy", "queaf", "raghead", "rape", "raping", "rapist", "rectum", "rimjob", "rimming", "rosy palm", "rusty trombone", "sadism", "scat", "schlong", "scissoring", "semen", "sex", "sexo", "sexy", "shaved beaver", "shaved pussy", "shemale", "shibari", "shit", "shota", "shrimping", "slanteye", "slut", "sluts", "s&m", "smut", "snatch", "sodomize", "sodomy", "spic", "spooge", "spread legs", "strap on", "strapon", "strip club", "style doggy", "suck", "sucks", "suicide girls", "sultry women", "swastika", "swinger", "tea bagging", "threesome", "throating", "tied up", "tight white", "tit", "tits", "titties", "titty", "topless", "tosser", "tranny", "tribadism", "tub girl", "tubgirl", "tushy", "twat", "twink", "twinkie", "undressing", "upskirt", "urethra play", "urophilia", "vagina", "venus mound", "vibrator", "violet wand", "vorarephilia", "voyeur", "vulva", "wank", "wetback", "wet dream", "white power", "women rapping", "wrapping men", "xx", "xxx", "yaoi", "yiffy", "zoophilia")
 filter <- tm_map(preproc, removeWords, profanities)
 
 # 6. Tokenizing 1-grams
 
 options(mc.cores = 1)
-UnigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 1, delimiters = " \\r\\n\\t.,;:\"()?!|"))
-unigrams <- tm_map(preproc, content_transformer(UnigramTokenizer))
+OnegramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 1, delimiters = " \\r\\n\\t.,;:\"()?!|"))
+onegrams <- tm_map(preproc, content_transformer(OnegramTokenizer))
 
 # 7. Tokenizing 2-grams
 
-BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2, delimiters = " \\r\\n\\t.,;:\"()?!|"))
-bigrams <- tm_map(preproc, content_transformer(BigramTokenizer))
+TwogramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2, delimiters = " \\r\\n\\t.,;:\"()?!|"))
+twograms <- tm_map(preproc, content_transformer(TwogramTokenizer))
 
 # 8. Tokenizing 3-grams
 
-TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3, delimiters = " \\r\\n\\t.,;:\"()?!|"))
-trigrams <- tm_map(preproc, content_transformer(TrigramTokenizer))
+ThreegramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3, delimiters = " \\r\\n\\t.,;:\"()?!|"))
+threegrams <- tm_map(preproc, content_transformer(ThreegramTokenizer))
 
 # 9. Tokenizing 4-grams
 
-QuadgramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 4, max = 4, delimiters = " \\r\\n\\t.,;:\"()?!|"))
-quadgrams <- tm_map(preproc, content_transformer(QuadgramTokenizer))
+FourgramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 4, max = 4, delimiters = " \\r\\n\\t.,;:\"()?!|"))
+fourgrams <- tm_map(preproc, content_transformer(FourgramTokenizer))
 
 # 10. Tokenizing 5-grams
 
-PentagramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 5, max = 5, delimiters = " \\r\\n\\t.,;:\"()?!|"))
-pentagrams <- tm_map(preproc, content_transformer(PentagramTokenizer))
+FivegramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 5, max = 5, delimiters = " \\r\\n\\t.,;:\"()?!|"))
+fivegrams <- tm_map(preproc, content_transformer(FivegramTokenizer))
